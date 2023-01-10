6f3671c02e2ace2398f26f8f9b2356cc0d9fa2cd
Jim Brennan <jim.j.brennan@gmail.com>
2023-01-10 12:32:36 -0500

    passed all the tests

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+};
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+function appendCat() {
+    let copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(cat) {
+    let copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    let copyOfCats = cats.slice(0, cats.length - 1)
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    let copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
